672ca86e5fb3d73f32e4fc0bd550222f52f5f165
Mahanthb <142413322+Mahanthb@users.noreply.github.com>
2024-06-24 11:04:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+  plugins: [
+    react(),
+  ],
   build: {
     rollupOptions: {
-      external: ['firebase/app', 'firebase/storage'],
+      external: ['react', 'react-dom', 'react-dropzone'], // Add 'react-dropzone' to external modules
     },
   },
 });
